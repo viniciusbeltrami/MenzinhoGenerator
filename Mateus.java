48c5c007592b50fd13bd86096eeab9c8a3ce5f3f
Vinícius Beltrami <vinicius.beltrami@shift.com.br>
2025-03-21 16:50:01 -0300

    adicionando menzinho mateus baroni

---
+++ Mateus.java
@@ -0,0 +1,20 @@
+public class Artur {
+    static final String name = "ARTUR TROMBIM DE SOUSA";
+    static final String currentPosition = "trainee and studant";
+    static final List<String> SomeSkills= ["Music","Teaching","Organization"];
+    static final List<String> 
+    languages = [ "Java", "C#", "Python","SQL" ];
+    }
+
+public class Mateus extends Artur {
+    static final String name = "MATEUS ROSOLEM BARONI";
+
+    static final List<String> SomeSkills = new ArrayList<String>() {{
+        add("Coding");
+        add("Goiabada");
+    }};
+    static final List<String> languages = new ArrayList<String>() {{
+        add("Airpods");
+        add("SmartFit");
+    }};
+}
