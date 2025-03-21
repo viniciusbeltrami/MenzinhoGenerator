d26750045ee358f0aee60adaf5c81d1ff2237698
Artur Trombim <114882647+Tuizim@users.noreply.github.com>
2025-03-21 17:12:19 -0300

    Update Artur.java

--- Artur.java
+++ Artur.java
@@ -1,12 +1,11 @@
-public class Artur {
-    static final String name = "ARTUR TROMBIM DE SOUSA";
-    static final String currentPosition = "trainee and studant";
-    static final List<String> SomeSkills= ["Music","Teaching","Organization"];
-    static final List<String> 
-    languages = [ "Java", "C#", "Python","SQL" ];
-    }
+class Artur {
+ static final String name = "ARTUR TROMBIM DE SOUSA";
+ static final String currentPosition = "Software Development Intern";
+ static final List<String> SomeSkills= ["Music","Teaching","Organization"];
+ static final List<String> languages = [ "Java", "C#", "Python","SQL" ];
+};
 
-    public class Mateus extends Artur {
+public class Mateus extends Artur {
         static final String name = "MATEUS ROSOLEM BARONI";
     
         static final List<String> SomeSkills = new ArrayList<String>() {{
