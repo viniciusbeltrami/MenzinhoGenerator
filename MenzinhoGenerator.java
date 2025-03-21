a0fef275b276a18517673cc728cdf1946549488a
Vinicius Beltrami -  Shift (Desenvolvimento) <vinicius.beltrami@shift.com.br>
2025-03-21 14:39:58 -0300

    MenzinhoGenerator

---
+++ MenzinhoGenerator.java
@@ -0,0 +1,7 @@
+public class Artur {
+    static final String name = "ARTUR TROMBIM DE SOUSA";
+    static final String currentPosition = "trainee and studant";
+    static final List<String> SomeSkills= ["Music","Teaching","Organization"];
+    static final List<String> 
+    languages = [ "Java", "C#", "Python","SQL" ];
+    }
